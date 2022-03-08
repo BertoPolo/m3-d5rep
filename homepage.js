dfd214c6a908d13a648731a581b4a6d5c1fe8770
BertoPolo <bertopolo91@gmail.com>
2022-03-08 11:20:20 +0100

    a

---
+++ homepage.js
@@ -0,0 +1,24 @@
+/* <a href="">
+                  <div
+                    class="card-container position-relative d-flex justify-content-center"
+                  >
+                    <img
+                      src="assets/playhoverbtn.png"
+                      class="playhover2"
+                      alt=""
+                    />
+                    <div>
+                      <img
+                        class="mt-3 img-card"
+                        src="assets/janis.png"
+                        alt="Card image cap"
+                      />
+                      <div
+                        class="d-flex flex-column align-items-start p-0 mt-3 ml-1 justify-content-start"
+                      >
+                        <div class="card-title line-clamp2">Janis Joplin</div>
+                        <p class="card-text mt-n2 line-clamp">Artist</p>
+                      </div>
+                    </div>
+                  </div>
+                </a> */
