04dd51af923f35d0c89b539dc35119bd2b96a078
BertoPolo <bertopolo91@gmail.com>
2022-03-08 10:40:41 +0100

    adsd

---
+++ ArtistPage.js
@@ -0,0 +1,49 @@
+const mouseoverSong = function () {
+  songNode = document.getElementsByClassName("song");
+  numNode = document.getElementsByClassName("listNumber");
+  heartNode = document.getElementsByClassName("heartSong");
+  threeDotsNode = document.getElementsByClassName("threeDotsSong");
+  for (let i = 0; i < songNode.length; i++) {
+    songNode[i].addEventListener("mouseover", function (e) {
+      songNode[i].classList.add("highLighting");
+      numNode[i].innerText = " ";
+      numNode[i].classList.add("listPlay");
+      heartNode[i].classList.remove("hidden");
+      threeDotsNode[i].classList.remove("hidden");
+    });
+    songNode[i].addEventListener("mouseleave", function (e) {
+      songNode[i].classList.toggle("highLighting");
+      numNode[i].innerText = i + 1;
+      numNode[i].classList.toggle("listPlay");
+      heartNode[i].classList.add("hidden");
+      threeDotsNode[i].classList.add("hidden");
+    });
+  }
+};
+mouseoverSong();
+
+const expander = function () {
+  const seeMoreNode = document.querySelector(".seeMore");
+  const hiddenSongNode = document.getElementsByClassName("hiddenSong");
+
+  seeMoreNode.addEventListener("click", function (e) {
+    for (let i = 0; i < hiddenSongNode.length; i++) {
+      hiddenSongNode[i].classList.remove("hiddenSong");
+    }
+  });
+};
+expander();
+
+// could be greater if it creates 5 songs instead unhide 5
+
+/* const loadData = (id) => {
+  fetch("https://striveschool-api.herokuapp.com/api/deezer/artist/" + id)
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+    });
+};
+
+window.onload = () => {
+  loadData(412);
+}; */
